994ae14791f18898483639b5497906b776d54440
orenswisa123 <47084527+orenswisa123@users.noreply.github.com>
2019-08-18 20:39:35 +0300

    Add files via upload

--- Bitwise/Bitwise.c
+++ Bitwise/Bitwise.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
-#include<limits.h>
+#include<string.h>
+#include <math.h>
+#include<stdlib.h>
+#include"Bitwise.h"
+
+
+static void toBinari(unsigned int v);
 
 char my_compliment(char c)
 {
-    char b=255;
+    unsigned int num;
+    unsigned char b=255;
     c=c^b;
+    num=c;
+    toBinari(num);
     return c;
 }
 char RotateByNum(char c,int n)
 {
     int i;
+    unsigned int num;
     char temp=c;
-    temp=tem<<(sizeof(char)-n);
+    temp=temp<<(sizeof(char)-n);
     c=c>>n;
     c=c|temp;
+    num=c;
+    toBinari(num);
     return c;
     
 }
 
 
-/*char RotateByNum(char c,int n)
-{
-    c=(x << n) | (x >> (8 - n);
-    return c;
-}*/
-int SetBitsOne(w,i,j,value)
+int SetBitsOne(char w,int i,int j,int value)
 {
     int index=1;
+    unsigned int num;
     int k;
     for(k=0;k<sizeof(int)*8-1;k++)
     {
@@ -35,13 +43,16 @@ int SetBitsOne(w,i,j,value)
     }
     index=index<<j;
     index=index>>i;
-    value=w|index;
-    return value;
+    w=w|index;
+    num=w;
+    toBinari(num);
+    return num;
 }
 
-int SetBitZero(w,i,j,value)
+int SetBitZero(char w,int i,int j,int value)
 {
     int index=1;
+    unsigned int num;
     int k;
     for(k=0;k<sizeof(int)*8-1;k++)
     {
@@ -51,5 +62,58 @@ int SetBitZero(w,i,j,value)
     index=index<<j;
     index=index>>i;
     index=~index;
-    value=w&index;
-    return value;
+    w=w&index;
+    num=w;
+    toBinari(num);
+    return num;
+}
+int setXYPN(unsigned char* x,int p,int n,unsigned char* y)
+{
+    int k;
+   unsigned int num;
+   unsigned char temp=1;
+       for(k=0;k<sizeof(unsigned char)*8-1;k++)
+    {
+        temp=temp<<1;
+        temp+=1;
+    }
+    if((x!=NULL)&&(y!=NULL))
+    {
+
+        temp = temp << (p - n);
+        temp = ~temp;
+        *x = *x & temp;
+        *y = *y << (8 - n);
+        *y = *y >> (8 - p);
+        *x = *y | *x;
+     
+        num=(unsigned int)(*x);
+        toBinari(num);
+    
+     return 1;
+   }
+  return 0;
+   
+}
+
+static void toBinari(unsigned int v){
+       char a[256];
+       int i=0,j=0;
+    
+       while(v>0){
+        a[i]=v%2+'0';
+         v=v/2;
+         i++;
+         
+       } 
+    a[i]='\0';
+         
+         for(i=8;i>=0;i--){
+            if(i<=strlen(a)){
+              printf("%c",a[i]);
+            }else{
+              printf("%c",'0');
+            }
+         }
+        
+}
---
+++ Bitwise/Bitwise.h
@@ -0,0 +1,12 @@
+#ifndef BITWISE_H
+#define BITWISE_H
+
+
+char my_compliment(char c);
+char RotateByNum(char c,int n);
+int SetBitsOne(char w,int i,int j,int value);
+int SetBitZero(char w,int i,int j,int value);
+int setXYPN(unsigned char* x,int p,int n,unsigned char* y);
+
+
+#endif
