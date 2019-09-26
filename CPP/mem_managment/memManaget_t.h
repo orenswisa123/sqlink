b93fbfd609c52c737416c8dd9ebc64b1683d780e
orenswisa123 <47084527+orenswisa123@users.noreply.github.com>
2019-09-26 16:10:41 +0300

    Add files via upload

--- CPP/mem_managment/memManaget_t.h
+++ CPP/mem_managment/memManaget_t.h
@@ -1,24 +1,22 @@
 #ifndef MEMMANAGER_H
 #define MEMMANAGER_H
+#include<string>
 
 class memManaget_t
 {
 public:
     memManager_t(); //default CTOR
-    bool isMemEmpty();
-    size_t getCurrentPosition();
-    size_t setCurrentPosition();
+    bool isMemEmpty()const;
+    size_t getCurrentPosition()const;
+    size_t setCurrentPosition(size_t);
+    size_t getactualSize()const;
 protected:
-    virtual size_t writeFuncInPlace(void*,size_t);//we get number of bytes
-    virtual size_t writeFuncOtherPlace(size_t position,void*,size_t);
-    virtual size_t readFuncInPlace(size_t)//return num of success bytes read
-    virtual size_t readFuncOtherPlace(size_t position,size_t);
 
-    ~memManaget_t();//DTOR
-
-    
-    size_t getactualSize();
-
+    virtual size_t write(const void*,size_t);//we get number of bytes
+    virtual size_t write(const void*,size_t,size_t position);
+    virtual size_t read(void*,size_t)//return num of success bytes read
+    virtual size_t read(void*,size_t,size_t position);
+    virtual ~memManaget_t();//DTOR
 
 private:
     size_t m_currentPosition;
